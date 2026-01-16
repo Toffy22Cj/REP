bcfeeb50a672a18fad2f8f5f06977e5c64cb5baa
Carlos J Pérez <cjpp12220@gmail.com>
2026-01-16 18:03:35 -0500

    empaquetador

--- src/main/java/com/rep/config/SpringFXMLLoader.java
+++ src/main/java/com/rep/config/SpringFXMLLoader.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -21,6 +20,7 @@ public class SpringFXMLLoader {
 
     /**
      * Carga un archivo FXML desde el classpath
+     * 
      * @param fxmlPath Ruta relativa del archivo FXML (ej: "/view/login.fxml")
      * @return Parent con la vista cargada
      * @throws IOException Si el archivo no se encuentra o hay errores de carga
@@ -31,8 +31,9 @@ public class SpringFXMLLoader {
             fxmlPath = "/" + fxmlPath;
         }
 
-        // Obtiene la URL del recurso usando ClassPathResource
-        URL fxmlUrl = new ClassPathResource(fxmlPath).getURL();
+        // Obtiene la URL del recurso usando getClass().getResource() que suele ser
+        // mejor para JavaFX
+        URL fxmlUrl = getClass().getResource(fxmlPath);
 
         if (fxmlUrl == null) {
             throw new IOException("Archivo FXML no encontrado: " + fxmlPath);
@@ -46,6 +47,7 @@ public class SpringFXMLLoader {
 
     /**
      * Versión alternativa que acepta URLs directamente
+     * 
      * @param fxmlUrl URL completa del archivo FXML
      * @return Parent con la vista cargada
      * @throws IOException Si hay errores de carga
